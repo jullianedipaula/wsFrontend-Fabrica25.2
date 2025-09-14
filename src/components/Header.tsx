492252f2fbaeb1ea6fc1e5f322b4aeb78fc2744f
Julliane <dipaulajulliane@gmail.com>
2025-09-14 03:11:33 -0300

    Componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="flex items-center justify-center">
+      <div className="">
+        <img src="/logo.svg" alt="Logo" className="h-50 w-70" />
+      </div>
+    </header>
+  );
+}
